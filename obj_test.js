20efdf2e3830fbd4eb76a9216931bb3c4b37404d
SinsilMathew <sinsilmathew@yahoo.com>
2018-01-10 19:00:33 +0530

    Object Mapping

--- obj_test.js
+++ obj_test.js
@@ -28,3 +28,21 @@ const obj={
     }
 };
 obj.add(100,20);
+
+obj6 = obj1; 
+obj6.a.b=3;
+console.log(obj6);
+console.log(obj1);
+obj7 =Object.assign(obj2);
+obj2.a.b=10;
+console.log(obj2);
+console.log(obj7);
+
+var a=5;
+var b;
+b=11;
+a=b;
+
+console.log(b)
+console.log(a);
+
